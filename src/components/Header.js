cc7ea24c9c16367b3217a6e4b5c0aecf5f92c071
Collin Chapman <cchap14@gmail.com>
2021-08-08 16:16:06 -0700

    add tickets image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
+import ticketsImage from "./../img/ticket.png";
 
 function Header(){
   return (
-    <h1>Help Queue</h1>
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <img src={ticketsImage} alt="an image of tickets" />
+    </React.Fragment>
+
   );
 }
 
