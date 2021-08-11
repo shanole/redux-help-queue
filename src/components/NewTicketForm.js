8998345734721652251b688d3408810af7686b75
Collin Chapman <cchap14@gmail.com>
2021-08-10 20:07:46 -0700

    add ReusableForm component to dry up code

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,24 +1,14 @@
 import React from "react";
 import { v4 } from 'uuid';
 import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
 
 function NewTicketForm(props){
   return (
     <React.Fragment>
-      <form onSubmit={handleNewTicketFormSubmission}>
-        <input
-          type="text"
-          name="names"
-          placeholder="Pair Names" />
-        <input
-          type="text"
-          name="location"
-          placeholder="Location" />
-        <textarea
-          name="issue"
-          placeholder="Describe your issue" />
-        <button type="submit">Help!</button>
-      </form>
+      <ReusableForm
+        formSubmissionHandler={handleNewTicketFormSubmission}
+        buttonText="Help!" />
     </React.Fragment>
   );
 
