d198bd964fef14a8f72b89a2264548222ab81d2c
Collin Chapman <cchap14@gmail.com>
2021-08-23 21:24:03 -0700

    add combined reducers to React

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -11,7 +11,7 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
+      // formVisibleOnPage: false,
       // masterTicketList: [],
       selectedTicket: null,
       editing: false
@@ -79,7 +79,11 @@ handleChangingSelectedTicket = (id) => {
       issue: issue,
     }
     dispatch(action);
-    this.setState({formVisibleOnPage: false})
+    const action2 = {
+      type: 'TOGGLE_FORM'
+    }
+    dispatch(action2)
+    // this.setState({formVisibleOnPage: false})
 
     // const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
     // this.setState({masterTicketList: newMasterTicketList,
@@ -90,14 +94,19 @@ handleChangingSelectedTicket = (id) => {
   handleClick = () => {
     if (this.state.selectedTicket != null) {
       this.setState({
-        formVisibleOnPage: false,
+        // formVisibleOnPage: false,
         selectedTicket: null,
         editing: false
       });
     } else {
-        this.setState(prevState => ({
-          formVisibleOnPage: !prevState.formVisibleOnPage // we pass in the current state of the formVisibleOnPage boolean to prevState. now that we know this value, we can say the new state should be the opposite of the old state
-      }));
+      const { dispatch } = this.props;
+      const action = {
+        type: 'TOGGLE_FORM'
+      }
+      dispatch(action);
+      //   this.setState(prevState => ({
+      //     formVisibleOnPage: !prevState.formVisibleOnPage // we pass in the current state of the formVisibleOnPage boolean to prevState. now that we know this value, we can say the new state should be the opposite of the old state
+      // }));
     }
   }
 
@@ -110,10 +119,10 @@ handleChangingSelectedTicket = (id) => {
       onEditTicket = {this.handleEditingTicketInList} />
       buttonText = "Return to Ticket List";
     } else if (this.state.selectedTicket != null) {
-      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket}                onClickingDelete = {this.handleDeletingTicket} 
+      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} onClickingDelete = {this.handleDeletingTicket} 
       onClickingEdit = {this.handleEditClick} />
       buttonText = "Return to Ticket List";
-    } else if (this.state.formVisibleOnPage) {
+    } else if (this.props.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm onNewTicketCreation = {this.handleAddingNewTicketToList} />;
       buttonText = "Return to Ticket List";
     } else {
@@ -131,12 +140,14 @@ handleChangingSelectedTicket = (id) => {
 }
 
 TicketControl.propTypes = {
-  masterTicketList: PropTypes.object
+  masterTicketList: PropTypes.object,
+  formVisibleOnPage: PropTypes.bool
 };
 
 const mapStateToProps = state => {
   return {
-    masterTicketList: state
+    masterTicketList: state.masterTicketList,
+    formVisibleOnPage: state.formVisibleOnPage
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers/ticket-list-reducer';
+// import reducer from './reducers/ticket-list-reducer';
+import rootReducer from './reducers/index';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 store.subscribe(() =>
   console.log(store.getState())
